72bca910ba079f7f59566cd4188f2bf92e963f39
Yanyao Wang <wangyanyao@msn.com>
2016-02-20 22:12:18 -0700

    The Second Version: modified add method, subtract method, getTotal method, multiply method and divide method.

--- Cse360_A3/src/cse360assign3/CalculatorTest.java
+++ Cse360_A3/src/cse360assign3/CalculatorTest.java
@@ -1,7 +1,6 @@
 package cse360assign3;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class CalculatorTest {
@@ -9,32 +8,68 @@ public class CalculatorTest {
 	@Test
 	public void testCalculator() {
 		Calculator c = new Calculator();
-		assertNotNull(c);
+		
+		assertNotNull("The Calculator is NULL", c);
 	}
 
 	@Test
 	public void testGetTotal() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		Calculator c = new Calculator();
+		
+		assertEquals(c.getTotal(), 0);
 	}
 
 	@Test
 	public void testAdd() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		Calculator c = new Calculator();
+		
+		c.add(5);
+		assertEquals("The total is not equal to 5!", c.getTotal(), 5);
+		
+		c.add(5);
+		assertEquals(c.getTotal(), 10);
 	}
 
 	@Test
 	public void testSubtract() {
-		fail("Not yet implemented");
+		Calculator c = new Calculator();
+		
+		c.add(5);
+		c.subtract(3);
+		assertEquals(c.getTotal(), 2);
+		
+		c.add(5);
+		c.subtract(3);
+		assertEquals(c.getTotal(), 4);
 	}
 
 	@Test
 	public void testMultiply() {
-		fail("Not yet implemented");
+		Calculator c = new Calculator();
+
+		c.add(5);
+		c.multiply(3);
+		assertEquals(c.getTotal(), 75);
 	}
 
 	@Test
 	public void testDivide() {
-		fail("Not yet implemented");
+		Calculator c = new Calculator();
+
+		c.add(15);
+		c.divide(3);
+		assertEquals(c.getTotal(), 5);
+	}
+	
+	@Test
+	public void testDivideByZero() {
+		Calculator c = new Calculator();
+
+		c.add(15);
+		c.divide(0);
+		assertEquals(c.getTotal(), 0);
 	}
 
 	@Test
